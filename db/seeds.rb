edd45d514610eecb2c4cfd6441d8572d919b5002
Althea Yi <altheaserena.yi@gmail.com>
2016-06-22 16:53:23 -0400

    Made the views and working index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Restaurant.create(name: "Mitch's Pizza", location: "15 King Street West", logo_url: "http://placekitten.com/100/100", site_url: "https://gentle-plains-55678.herokuapp.com", description: "Mitch's pizza is the best pizza! EVA!", phone: '647-666-6666', capacity: 30)
