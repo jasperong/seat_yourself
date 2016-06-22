d18bbdf6683ed65292065d371ffc76d87bd7ae3e
Althea Yi <altheaserena.yi@gmail.com>
2016-06-22 17:28:24 -0400

    Created Reservation model and associations

---
+++ db/migrate/20160622212621_create_reservations.rb
@@ -0,0 +1,13 @@
+class CreateReservations < ActiveRecord::Migration
+  def change
+    create_table :reservations do |t|
+      t.datetime :time
+      t.integer :party_size
+      t.text :notes
+      t.belongs_to :user, index: true, foreign_key: true
+      t.belongs_to :restaurant, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
