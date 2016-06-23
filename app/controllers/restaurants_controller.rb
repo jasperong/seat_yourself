b0c2c03bd6670e6e5ee796d354a0895deb552423
Jasper <jave.ong4@yahoo.com>
2016-06-23 15:37:35 -0400

    Create new restaurant form

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,4 +7,18 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find(params[:id])
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/edit'
-
-  get 'users/update'
-
-  get 'users/destroy'
 
   root 'restaurants#index'
-  resources :restaurants, only: :show do
+  resources :restaurants do
     resources :reservations, only: [:create, :new, :show]
   end
   resources :sessions, only: [:new, :create, :destroy]
