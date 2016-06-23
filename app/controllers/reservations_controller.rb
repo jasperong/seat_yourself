8008eb27ff50d3ab785e1a5145d9bab8df7014a0
Mitch Pateman <mitch_pateman@hotmail.com>
2016-06-23 17:15:22 -0400

    made an edit page view for reservation and an entire reservation view folder... added @reservations line in the reservations controller under Edit

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,50 +2,52 @@ class ReservationsController < ApplicationController
 
 before_action :load_restaurant
 
-  def new
-  end
+	def new
+	end
 
-  def create
-    @reservation = @restaurant.reservations.build(reservation_params)
-    if @reservation.save
-      flash[:notice] = "Reservation successful"
-      redirect_to restaurant_path(@restaurant)
-    else
-      flash[:notice] = "Error: Did not work fool."
-      render :new
-    end
-  end
+	def create
+		@reservation = @restaurant.reservations.build(reservation_params)
+		if @reservation.save
+			flash[:notice] = "Reservation successful"
+			redirect_to restaurant_path(@restaurant)
+		else
+			flash[:notice] = "Error: Did not work fool."
+			render :new
+		end
+	end
 
-  def show
-    @reservation = Reservation.find(params[:id])
-  end
+	def show
+		@reservation = Reservation.find(params[:id])
+	end
 
-  def edit
-    @reservation = Reservation.find(params[:id])
-  end
+	def edit
+		@reservation = Reservation.find(params[:id])
+		@reservations = Reservation.where(restaurant_id: @restaurant.id)
 
-  def update
-    @reservation = Reservation.find(params[:id])
-    if @reservation.update_attributes(reservation_params)
-      redirect_to @restaurant
-    else
-      render :edit
-    end
-  end
+	end
 
-  def destroy
-    @reservation = Reservation.find(params[:id])
-    @reservation.destroy
-    redirect_to @restaurant
-  end
+	def update
+		@reservation = Reservation.find(params[:id])
+		if @reservation.update_attributes(reservation_params)
+			redirect_to @restaurant
+		else
+			render :edit
+		end
+	end
 
-  private
-  def reservation_params
-    params.require(:reservation).permit(:time, :party_size, :notes)
-  end
+	def destroy
+		@reservation = Reservation.find(params[:id])
+		@reservation.destroy
+		redirect_to @restaurant
+	end
 
-  def load_restaurant
-    @restaurant = Restaurant.find(params[:restaurant_id])
-  end
+	private
+	def reservation_params
+		params.require(:reservation).permit(:time, :party_size, :notes)
+	end
+
+	def load_restaurant
+		@restaurant = Restaurant.find(params[:restaurant_id])
+	end
 
 end
