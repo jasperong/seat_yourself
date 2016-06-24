ff7824d0eb8badce8354afe2a107fb5f7262c21e
Jasper <jave.ong4@yahoo.com>
2016-06-24 10:05:36 -0400

    Added validation for reservations. testing needed

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,22 @@
 class Reservation < ActiveRecord::Base
-	belongs_to :user
-	belongs_to :restaurant
-	validates :time, :party_size, presence: true
+  belongs_to :user
+  belongs_to :restaurant
+  validates :time, :party_size, presence: true
+  validates :reservation_under_capacity
+
+
+  def reservation_under_capacity
+    total_size = []
+    restaurant = Restaurant.find(params[:id])
+
+    restaurant.reservations.each do |reservation|
+      total_size << reservation.party_size
+    end
+
+    if restaurant.capacity < total_size
+      errors.add(restaurant: "is overbooked!!")
+    end
+  end
+
+
 end
