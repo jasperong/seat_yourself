715006d95d0f2731f4a117e7961b7855eb876f7e
Mitch Pateman <mitch_pateman@hotmail.com>
2016-06-24 15:19:13 -0400

    Added open and close_hour and category for restaurants FOR REAL

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160624183343) do
+ActiveRecord::Schema.define(version: 20160624191601) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "cuisine"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20160624183343) do
     t.datetime "updated_at",  null: false
     t.integer  "open_hour"
     t.integer  "close_hour"
+    t.string   "category"
   end
 
   create_table "users", force: :cascade do |t|
