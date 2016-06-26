7b873d998327da2848fd9c247fa6847aecbeb808
Althea Yi <altheaserena.yi@gmail.com>
2016-06-26 15:54:01 -0400

    Got flashes working on Edit Restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,55 +1,57 @@
 class RestaurantsController < ApplicationController
 
-	def index
-		@restaurants = Restaurant.all
-	end
+  def index
+    @restaurants = Restaurant.all
+  end
 
-	def show
-		@restaurant = Restaurant.find(params[:id])
-		@reservation = @restaurant.reservations.build
-		@reservations = Reservation.where(restaurant_id: @restaurant.id)
-	end
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @reservation = @restaurant.reservations.build
+    @reservations = Reservation.where(restaurant_id: @restaurant.id)
+  end
 
 
-	def new
-		@restaurant = Restaurant.new
-		@categories = Category.all
-	end
+  def new
+    @restaurant = Restaurant.new
+    @categories = Category.all
+  end
 
-	def create
-		@restaurant = Restaurant.new(restaurant_params)
-		if @restaurant.save
-			redirect_to restaurant_path(@restaurant)
-		else
-			render new_restaurant_path
-		end
-	end
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      flash[:notice] = "Restaurant Successfully Created!"
+      redirect_to restaurant_path(@restaurant)
+    else
+      flash[:alert] = "Restaurant not created foo!"
+      render new_restaurant_path
+    end
+  end
 
-	def edit
-		@restaurant = Restaurant.find(params[:id])
-	end
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
 
-	def update
-		@restaurant = Restaurant.find(params[:id])
-		if @restaurant.update_attributes(restaurant_params)
-			flash[:notice] = "Restaurant Successfully Edited!"
-			redirect_to @restaurant
-		else
-			render :edit
-		end
-	end
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    if @restaurant.update_attributes(restaurant_params)
+      flash[:notice] = "Restaurant Successfully Edited!"
+      redirect_to @restaurant
+    else
+      render :edit
+    end
+  end
 
-	def destroy
-			@restaurant = Restaurant.find(params[:id])
-			@restaurant.destroy
-			flash[:notice] = "Restaurant Successfully Removed! Foo!"
-			redirect_to root_path
-	end
+  def destroy
+      @restaurant = Restaurant.find(params[:id])
+      @restaurant.destroy
+      flash[:notice] = "Restaurant Successfully Removed! Foo!"
+      redirect_to root_path
+  end
 
-	private
-	def restaurant_params
-		params.require(:restaurant).permit(:name, :location, :logo_url, :site_url, :phone, :description, :capacity, :open_hour, :close_hour, :category)
-	end
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :location, :logo_url, :site_url, :phone, :description, :capacity, :open_hour, :close_hour, :category)
+  end
 
 
 end
