1a9bfa3afec982f62c361afda1595adb5f17cae1
Jasper <jave.ong4@yahoo.com>
2016-06-26 14:27:46 -0400

    Overhauled migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160624191601) do
+ActiveRecord::Schema.define(version: 20160626181658) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "cuisine"
