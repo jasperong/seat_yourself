35f0546acdc5c40dae62555442f1645fab11db4c
Jasper <jave.ong4@yahoo.com>
2016-06-27 19:13:24 -0400

    Refactor code i.e. indentation/spacing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      flash[:notice] = "Successfully signed up!!"
+      flash[:notice] = "Successfully signed up and logged in!!"
       redirect_to root_path
     else
       render :new
