0dce0fa83fa93d91adc1e683c9f68173ca5cd782
Althea Yi <altheaserena.yi@gmail.com>
2016-06-27 15:13:47 -0400

    Got reservations working on user profile

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -8,6 +8,7 @@ before_action :find_reservation, only: [:show, :edit, :update, :destroy]
 
   def create
     @reservation = @restaurant.reservations.build(reservation_params)
+    @reservation.user_id = current_user.id
     if @reservation.save
       flash[:notice] = "Reservation successful"
       redirect_to restaurant_path(@restaurant)
@@ -40,7 +41,7 @@ before_action :find_reservation, only: [:show, :edit, :update, :destroy]
 
   private
   def reservation_params
-    params.require(:reservation).permit(:time, :party_size, :notes)
+    params.require(:reservation).permit(:time, :party_size, :notes, :user_id)
   end
 
   def load_restaurant
