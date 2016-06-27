97d015652c265243468f1095ef1d89970b265ff8
Jasper <jave.ong4@yahoo.com>
2016-06-27 16:00:38 -0400

    Commit before merge

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,16 +4,16 @@ before_action :load_restaurant
 before_action :find_reservation, only: [:show, :edit, :update, :destroy]
 
   def new
+    @reservation = @restaurant.reservations.build(reservation_params)
   end
 
   def create
     @reservation = @restaurant.reservations.build(reservation_params)
-    @reservation.user_id = current_user.id
     if @reservation.save
       flash[:notice] = "Reservation successful"
       redirect_to restaurant_path(@restaurant)
     else
-      flash[:notice] = "Restaurant is full."
+      flash[:notice] = "Not the error I'm looking for."
       redirect_to restaurant_path(@restaurant)
     end
   end
@@ -41,7 +41,7 @@ before_action :find_reservation, only: [:show, :edit, :update, :destroy]
 
   private
   def reservation_params
-    params.require(:reservation).permit(:time, :party_size, :notes, :user_id)
+    params.require(:reservation).permit(:time, :party_size, :notes)
   end
 
   def load_restaurant
