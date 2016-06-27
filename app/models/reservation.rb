2cdd2e12d6ce92582a31c33d1b441a097f24a64d
Jasper <jave.ong4@yahoo.com>
2016-06-27 14:26:52 -0400

    Categories working now

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,18 +4,6 @@ class Reservation < ActiveRecord::Base
   validates :time, :party_size, presence: true
   validate :is_restaurant_available, :is_restaurant_open, :is_party_size_valid
 
-  # def reservation_under_capacity
-  #   total_size = []
-  #   restaurant = Restaurant.find(params[:id])
-  #
-  #   restaurant.reservations.each do |reservation|
-  #     total_size << reservation.party_size
-  #   end
-  #
-  #   if restaurant.capacity < total_size.inject(:+)
-  #     errors.add(restaurant: "is overbooked!!")
-  #   end
-  # end
   def is_restaurant_available
     # restaurant = Restaurant.find(params[:id])
     errors.add(:restaurant, "is overbooked foo!!") unless restaurant.available?(party_size, time)
@@ -23,7 +11,7 @@ class Reservation < ActiveRecord::Base
 
   # Make sure restaurant has open_hour and close_hour
   def is_restaurant_open
-    time > restaurant.open_hour && time < restaurant.close_hour
+    errors.add(:restaurant, "is closed!") unless time > restaurant.open_hour && time < restaurant.close_hour
   end
 
   def is_party_size_valid
